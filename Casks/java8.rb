7e6a7a3d43b93375d1f044107a02f58bf3ac8788
mojothejojo <theutonium.18@gmail.com>
2021-04-13 12:22:36 +0530

    Update java8.rb

--- Casks/java8.rb
+++ Casks/java8.rb
@@ -2,7 +2,7 @@ cask "java8" do
   version "16,36:7863447f0ab643c585b9bdebf67c69db"
   sha256 "90e762e0d9257623bf31d5f9c1b9572485c99565040f21d260220754c96a20e0"
 
-  url "https://enos.itcollege.ee/~jpoial/allalaadimised/jdk8/jdk-8u281-macosx-x64.dmg",
+  url "https://enos.itcollege.ee/~jpoial/allalaadimised/jdk8/jdk-8u281-macosx-x64.dmgo",
       cookies: {
         "oraclelicense" => "accept-securebackup-cookie",
       }
